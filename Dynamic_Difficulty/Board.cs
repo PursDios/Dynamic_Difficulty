1434fa28bec1e54fd7c5a6e3ec98096cdaf9974b
Ryan Cooper <pursdios942@gmail.com>
2018-12-19 20:01:50 +0000

    Split content up into board to allow it to handle the majority of the content being displayed.
    Added some basic functionality to the user choice method.

---
+++ Dynamic_Difficulty/Board.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Difficulty
+{
+    public class Board
+    {
+        private List<char> m_Arr;
+        public Board(List<char> p_Arr)
+        {
+            m_Arr = p_Arr;
+        }
+
+        public void LoadBoard()
+        {
+            Console.Clear();
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", m_Arr[0], m_Arr[1], m_Arr[2]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", m_Arr[3], m_Arr[4], m_Arr[5]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", m_Arr[6], m_Arr[7], m_Arr[8]);
+
+            Console.WriteLine("     |     |      ");
+        }
+
+        public void UpdateBoard(List<char> p_Arr)
+        {
+            m_Arr = p_Arr;
+            Console.Clear();
+            LoadBoard();
+        }
+    }
+}
--- Dynamic_Difficulty/Gameplay.cs
+++ Dynamic_Difficulty/Gameplay.cs
@@ -4,57 +4,34 @@ using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Dynamic_Difficulty
 {
     public class Gameplay
     {
+        private Board b;
         private char m_Turn = 'x';
         private bool m_End = false;
         private bool m_Dynamic = false;
+        private int m_Choice;
         private List<char> m_Arr;
-
-        public Gameplay()
-        {
-
-        }
 
         public void StartGame(bool dynamic)
         {
             m_Arr = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            b = new Board(m_Arr);
             m_Dynamic = dynamic;
 
             while (m_End == false)
             {
-                LoadBoard();
+                b.LoadBoard();
                 UserTakeTurn();
                 if (m_Dynamic)
                     DynamicTakeTurn();
                 else
                     StaticTakeTurn();
             }
-        }
-
-        private void LoadBoard()
-        {
-            Console.Clear();
-            Console.WriteLine("     |     |      ");
-
-            Console.WriteLine("  {0}  |  {1}  |  {2}", m_Arr[0], m_Arr[1], m_Arr[2]);
-
-            Console.WriteLine("_____|_____|_____ ");
-
-            Console.WriteLine("     |     |      ");
-
-            Console.WriteLine("  {0}  |  {1}  |  {2}", m_Arr[3], m_Arr[4], m_Arr[5]);
-
-            Console.WriteLine("_____|_____|_____ ");
-
-            Console.WriteLine("     |     |      ");
-
-            Console.WriteLine("  {0}  |  {1}  |  {2}", m_Arr[6], m_Arr[7], m_Arr[8]);
-
-            Console.WriteLine("     |     |      ");
         }
 
         private void CheckWin()
@@ -73,55 +50,66 @@ namespace Dynamic_Difficulty
                 m_End = true;
             }
             //bottom left to bottom right
-            else if(m_Arr[6] == m_Arr[7] && m_Arr[7] == m_Arr[8])
+            else if (m_Arr[6] == m_Arr[7] && m_Arr[7] == m_Arr[8])
             {
                 Console.WriteLine("{0}'s wins!", m_Arr[6]);
                 m_End = true;
             }
 
             //top left to bottom right
-            else if(m_Arr[0] == m_Arr[4] && m_Arr[4] == m_Arr[8])
+            else if (m_Arr[0] == m_Arr[4] && m_Arr[4] == m_Arr[8])
             {
                 Console.WriteLine("{0}'s wins!", m_Arr[0]);
                 m_End = true;
             }
             //top right to bottom left
-            else if(m_Arr[2] == m_Arr[4] && m_Arr[6] == m_Arr[4])
+            else if (m_Arr[2] == m_Arr[4] && m_Arr[6] == m_Arr[4])
             {
                 Console.WriteLine("{0}'s wins!", m_Arr[2]);
                 m_End = true;
             }
 
             //top left to bottom left
-            else if(m_Arr[0] == m_Arr[3] && m_Arr[3] == m_Arr[6])
+            else if (m_Arr[0] == m_Arr[3] && m_Arr[3] == m_Arr[6])
             {
                 Console.WriteLine("{0}'s wins!", m_Arr[0]);
                 m_End = true;
             }
             //top middle to bottom middle
-            else if(m_Arr[1] == m_Arr[4] && m_Arr[4] == m_Arr[7])
+            else if (m_Arr[1] == m_Arr[4] && m_Arr[4] == m_Arr[7])
             {
                 Console.WriteLine("{0}'s wins!", m_Arr[1]);
                 m_End = true;
             }
             //top right to bottom right
-            else if(m_Arr[2] == m_Arr[5] && m_Arr[5] == m_Arr[8])
+            else if (m_Arr[2] == m_Arr[5] && m_Arr[5] == m_Arr[8])
             {
                 Console.WriteLine("{0}'s wins!", m_Arr[2]);
                 m_End = true;
             }
 
             //checks for a draw
-            else if(m_Arr[1] != '1' && m_Arr[2] != '2' && m_Arr[3] != '3' && m_Arr[4] != '4' && m_Arr[5] != '5' && m_Arr[6] != '6' && m_Arr[7] != '7' && m_Arr[8] != '8' && m_Arr[9] != '9')
+            else if (m_Arr[1] != '1' && m_Arr[2] != '2' && m_Arr[3] != '3' && m_Arr[4] != '4' && m_Arr[5] != '5' && m_Arr[6] != '6' && m_Arr[7] != '7' && m_Arr[8] != '8' && m_Arr[9] != '9')
             {
                 Console.WriteLine("Draw");
                 m_End = true;
             }
         }
 
-        private static void UserTakeTurn()
+        private void UserTakeTurn()
         {
             //TODO
+            m_Choice = -1;
+            Console.WriteLine("Where would you like to go (1-9)");
+            int.TryParse(Console.ReadLine(), out m_Choice);
+            if (m_Choice != -1)
+            {
+                if (CheckChoice())
+                {
+                    ApplyChoice();
+                }
+            }
+
         }
 
         private static void StaticTakeTurn()
@@ -134,5 +122,17 @@ namespace Dynamic_Difficulty
             //TODO
         }
 
+        public  bool CheckChoice()
+        {
+            if (m_Arr[m_Choice - 1] == 'X' || m_Arr[m_Choice - 1] == 'O')
+                return false;
+            else
+                return true;
+        }
+
+        protected void ApplyChoice()
+        {
+            //TODO
+        }
     }
 }
